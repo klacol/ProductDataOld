f1027742589792135b6794c80df6d0d200eb9e6f
klacol <klaus.aengenvoort@etask.de>
2016-11-02 10:03:01 +0100

    renamed project

--- src/CafmConnectProductDataGui/Properties/AssemblyInfo.cs
+++ src/CafmConnectProductDataGui/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CafmConnectObjectsGui")]
+[assembly: AssemblyTitle("CafmConnectProductDataGui")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CafmConnectObjectsGui")]
+[assembly: AssemblyProduct("CafmConnectProductDataGui")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
